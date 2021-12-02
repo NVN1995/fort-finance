637d4775f67b20da4211903a90eca86c9c6349e7
NhanNguyen <nhannguyenqn1995@gmail.com>
2021-12-02 10:07:38 +0700

    add some command for js and css and index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,15 +7,22 @@ function showHideSideBar() {
   var sideBarHideItems = document.querySelectorAll(".js-sidebar-hide");
   var mainPage = document.querySelector(".js-mainpage");
 
+  // get sidebar default Width
   let sideBarWidth = sideBar.offsetWidth;
+  // when user click on Breadcrum Icon
   breadcrumIcon.addEventListener("click", function () {
+    // in case sidebar is showing
     if (sideBar.offsetWidth === sideBarWidth) {
+      // rotate icon
       breadcrumIcon.style.transform = "rotate(180deg)";
+      // minimize mainlogo
       mainLogo.style.width = "80px";
       mainLogoImg.style = "padding-left: 16px";
+      // minimize sidebar
       sideBar.style.width = "80px";
       sideBarContent.style = "margin-left: 6px";
       mainPage.style = "margin-left: 80px";
+      // hide sidebar content
       for (i = 0; i < sideBarHideItems.length; i++) {
         sideBarHideItems[i].classList.add("hide");
       }
@@ -33,28 +40,6 @@ function showHideSideBar() {
   });
 }
 
-// function clickMovingTab() {
-//   var mainContent = document.querySelector(".js-mainpage-content");
-//   var mainMovingTabBtns = mainContent.querySelectorAll(
-//     ".js-main-moving-tab-btn"
-//   );
-//   var mainContentItems = mainContent.querySelectorAll(".js-main-content-item");
-
-//   for (let i = 0; i < mainMovingTabBtns.length; i++) {
-//     mainMovingTabBtns[i].addEventListener("click", () => {
-//       for (let k = 0; k < mainMovingTabBtns.length; k++) {
-//         mainMovingTabBtns[k].classList.remove("active");
-//       }
-//       mainMovingTabBtns[i].classList.add("active");
-//       for (let j = 0; j < mainContentItems.length; j++) {
-//         mainContentItems[j].classList.add("hide");
-//         mainContentItems[j].classList.remove("show");
-//       }
-//       mainContentItems[i].classList.add("show");
-//     });
-//   }
-// }
-
 var mainContent = document.querySelector(".js-mainpage-content");
 var mainMovingTabBtns = mainContent.querySelectorAll(".js-main-moving-tab-btn");
 var mainContentItems = mainContent.querySelectorAll(".js-main-content-item");
@@ -63,13 +48,19 @@ var nftContent = mainContent.querySelector(".js-nft-content");
 var nftMovingTabBtns = nftContent.querySelectorAll(".js-nft-moving-tab-btn");
 var nftContentItems = nftContent.querySelectorAll(".js-nft-content-item");
 
+// function for Moving Tabs
 function clickMovingTab(movingTabBtns, contentItems) {
+  // get all button for adding 'click' event
   for (let i = 0; i < movingTabBtns.length; i++) {
     movingTabBtns[i].addEventListener("click", () => {
+      // remove 'active' status of all button
       for (let k = 0; k < movingTabBtns.length; k++) {
         movingTabBtns[k].classList.remove("active");
       }
+      // add 'active' status for Clicked button
       movingTabBtns[i].classList.add("active");
+
+      // show content for each button
       for (let j = 0; j < contentItems.length; j++) {
         contentItems[j].classList.add("hide");
         contentItems[j].classList.remove("show");
@@ -83,5 +74,6 @@ window.onload = () => {
   showHideSideBar();
   clickMovingTab(mainMovingTabBtns, mainContentItems);
   clickMovingTab(nftMovingTabBtns, nftContentItems);
+  // scroll to top when reload page
   window.scrollTo(0, 0);
 };
